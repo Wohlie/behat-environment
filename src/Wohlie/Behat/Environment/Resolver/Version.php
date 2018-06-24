30826241b3bd199b4fc4a12870afa82539c5b3f4
Erik Wohllebe <Erik.Wohllebe@googlemail.com>
2018-06-24 16:30:36 +0200

    Add license header to files.

--- src/Wohlie/Behat/Environment/Resolver/Version.php
+++ src/Wohlie/Behat/Environment/Resolver/Version.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * This file is part of the Behat Environment Extension.
+ * (c) Erik Wohllebe <Erik.Wohllebe@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Wohlie\Behat\Environment\Resolver;
 
